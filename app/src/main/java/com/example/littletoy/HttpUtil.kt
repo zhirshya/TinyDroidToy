2b7eb669929605f94b7998be5357d4f338786820
Zhirshya <prsvrrgtd@outlook.com>
2019-07-09 12:17:34 +0800

    first commit

---
+++ app/src/main/java/com/example/littletoy/HttpUtil.kt
@@ -0,0 +1,139 @@
+package com.example.littletoy
+
+import android.os.Handler
+import android.os.Message
+import android.view.View
+import android.widget.Button
+import java.io.BufferedReader
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.OutputStream
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import java.net.URLEncoder
+
+/**
+ * Created by Jason_Jan on 2017/7/5.
+ */
+
+object HttpUtil {
+
+    private var handler: Handler? = null
+    private var my_result: String? = null
+
+    fun httpGet(strUrlPath: String, params: Map<String, String>, encode: String): String {
+        var strUrlPath = strUrlPath
+        /* byte[] data = getRequestData(params, encode).toString().getBytes();//获得请求体*/
+        /* String target="http://emb.mobi/register";*/
+        var result: String? = null
+        val append_url = getRequestData(params, encode).toString()
+        strUrlPath = strUrlPath + "?" + append_url
+        try {
+            val url = URL(strUrlPath)
+            val urlConn = url.openConnection() as HttpURLConnection
+            urlConn.connectTimeout = 5000//超时时间
+            urlConn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")//设置头部信息，其实没什么用
+
+            //主角开始登场，不注意就是几个小时的调试，输入流
+            val `in` = InputStreamReader(urlConn.inputStream)
+
+            val buffer = BufferedReader(`in`)
+            var inputLine: String? = null
+            //循环逐行读取输入流中的内容
+
+            result = ""//每次清空数据
+
+            while (buffer.readLine().apply { inputLine = this } != null) {
+                result += inputLine!! + "\n"
+            }
+
+
+            `in`.close()
+            urlConn.disconnect()
+
+
+        } catch (e: MalformedURLException) {
+            e.printStackTrace()
+        } catch (ioe: IOException) {
+            ioe.printStackTrace()
+            return "err:" + ioe.message.toString()
+        }
+
+        return result!!
+
+    }
+
+
+    private fun getRequestData(params: Map<String, String>, encode: String): StringBuffer {
+        val stringBuffer = StringBuffer()        //存储封装好的请求体信息
+        try {
+            for ((key, value) in params) {
+                stringBuffer.append(key)
+                    .append("=")
+                    .append(URLEncoder.encode(value, encode))
+                    .append("&")
+            }
+            stringBuffer.deleteCharAt(stringBuffer.length - 1)    //删除最后的一个"&"
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return stringBuffer
+    }
+
+
+    fun httpPost(strUrlPath: String, params: Map<String, String>, encode: String): String {
+        val data = getRequestData(params, encode).toString().toByteArray()
+        try {
+            val url = URL(strUrlPath)
+
+            val http = url.openConnection() as HttpURLConnection
+            http.connectTimeout = 5000
+            http.doInput = true
+            http.doOutput = true
+            http.requestMethod = "POST"
+            http.useCaches = false//使用post方式不能用缓存
+            //设置请求体的类型是文本类型
+            http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+            //设置请求体的长度
+            http.setRequestProperty("Content-Length", data.size.toString())
+            //获得输出流，向服务器写入数据
+            val out = http.outputStream
+            out.write(data)
+
+            val response = http.responseCode
+            if (response == HttpURLConnection.HTTP_OK) {
+                val inputStream = http.inputStream
+                return dealResponseResult(inputStream)
+            }
+
+
+        } catch (ioe: IOException) {
+            ioe.printStackTrace()
+            return "err:" + ioe.message.toString()
+        }
+
+        return "-1"
+    }
+
+
+    fun dealResponseResult(inputStream: InputStream): String {
+        var resultData: String? = null      //存储处理结果
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        val data = ByteArray(1024)
+        var len = 0
+        try {
+            while (inputStream.read(data).apply { len = this } != -1) {
+                byteArrayOutputStream.write(data, 0, len)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        resultData = String(byteArrayOutputStream.toByteArray())
+        return resultData
+    }
+}
---
+++ app/src/main/java/com/example/littletoy/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.littletoy
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import com.google.gson.Gson
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private final var payload: Map<String, String>  = mapOf("Amount" to "1", "From" to "USD", "To" to "CNY")
+
+//    private var r: Response?
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//            var str:String = URL("http://httpbin.org/ip").readText()
+//            Get our IP
+
+        object : Thread() {
+            override fun run() {
+                println(HttpUtil.httpGet("http://httpbin.org/ip",payload,"utf-8"))
+                println(HttpUtil.httpGet("https://www.xe.com/currencyconverter/convert/",payload,"utf-8"))
+            }
+        }.start()
+//            // Get our IP in a simpler way
+//            println(get("http://icanhazip.com").text)
+
+//        setSupportActionBar(toolbar)
+//            var r = get("https://www.xe.com/currencyconverter/convert/", params = payload)
+//            var cbtn = findViewById<Button>(R.id.cvtBtn)
+//        cbtn.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
+        }
+
+    fun convertCurrency(view: View) {
+        if(waliutText.text.isNotEmpty()) {
+            val waliutValue = waliutText.text.toString().toFloat()
+            //https://www.xe.com/currencyconverter/convert/?Amount=1&From=USD&To=CNY
+//            waliutText2.setText(r?.text ?: "erroneous response")
+//            waliutText2.setText(r.text)
+        }else {
+            waliutText2.setText("")
+        }
+    }
+
+    init {
+//    https://khttp.readthedocs.io/en/latest/user/quickstart.html
+//        khttp is an elegant and simple HTTP library for Kotlin and Java, built for human beings.
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        println("asdfasdfasdfasdf")
+        return when (item.itemId) {
+            R.id.start -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
